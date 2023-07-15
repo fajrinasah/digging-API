9c7cd3456c8361d4c790f0caae8ff41ff1197a7a
fajrinasah <fajrinasah@gmail.com>
2023-07-15 22:07:27 +0700

    cleaned up codes

--- src/models/associations.js
+++ src/models/associations.js
@@ -7,33 +7,6 @@ import { User } from "./user.js";
 // import { Status } from "./status.js";
 
 export const associations = () => {
-  // // USER & ROLE
-  // Role.hasMany(User);
-
-  // User.belongsTo(Role, {
-  //   targetKey: "id",
-  //   foreignKey: "role_id",
-  // });
-
-  // /*-----------------------------------------*/
-  // // USER & STATUS
-  // Status.hasMany(User);
-
-  // User.belongsTo(Status, {
-  //   targetKey: "id",
-  //   foreignKey: "status_id",
-  // });
-
-  /*-----------------------------------------*/
-
-  // USER & PROFILE
-  // User.hasOne(Profile, {
-  //   sourceKey: "id",
-  //   foreignKey: "user_id",
-  //   onUpdate: "CASCADE",
-  //   onDelete: "CASCADE",
-  // });
-
   User.hasOne(Profile);
 
   Profile.belongsTo(User, {
@@ -43,15 +16,6 @@ export const associations = () => {
   });
 
   /*-----------------------------------------*/
-
-  // PROFILE & ARTICLE
-  // Profile.hasMany(Article, {
-  //   foreignKey: {
-  //     name: "profile_id",
-  //   },
-  //   onUpdate: "CASCADE",
-  //   onDelete: "CASCADE",
-  // });
 
   Profile.hasMany(Article);
 
@@ -63,15 +27,6 @@ export const associations = () => {
 
   /*-----------------------------------------*/
 
-  // CATEGORY & ARTICLE
-  // Category.hasMany(Article, {
-  //   foreignKey: {
-  //     name: "category_id",
-  //   },
-  //   onUpdate: "CASCADE",
-  //   onDelete: "SET NULL",
-  // });
-
   Category.hasMany(Article);
 
   Article.belongsTo(Category, {
@@ -82,15 +37,6 @@ export const associations = () => {
 
   /*-----------------------------------------*/
 
-  // PROFILE & CONSERVATION
-  // Profile.hasMany(Conservation, {
-  //   foreignKey: {
-  //     name: "conservator_id",
-  //   },
-  //   onUpdate: "CASCADE",
-  //   onDelete: "CASCADE",
-  // });
-
   Profile.hasMany(Conservation);
 
   Conservation.belongsTo(Profile, {
@@ -100,15 +46,6 @@ export const associations = () => {
   });
 
   /*-----------------------------------------*/
-
-  // ARTICLE & CONSERVATION
-  // Article.hasMany(Conservation, {
-  //   foreignKey: {
-  //     name: "article_id",
-  //   },
-  //   onUpdate: "CASCADE",
-  //   onDelete: "CASCADE",
-  // });
 
   Article.hasMany(Conservation);
 
