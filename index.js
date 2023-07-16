dd797dcca846fcf04791000c7ed8f0851c9c3f60
fajrinasah <fajrinasah@gmail.com>
2023-07-16 18:40:01 +0700

    updated app root file

--- index.js
+++ index.js
@@ -2,23 +2,34 @@ import express from "express";
 import bodyParser from "body-parser";
 import dotenv from "dotenv";
 import cors from "cors";
-import db from "./src/models/index.js";
+import * as middlewares from "./src/middlewares/index.js";
+import db from "./src/database/index.js";
 // import { associations } from "./src/models/associations.js";
 
 import chalk from "chalk";
 import * as emoji from "node-emoji";
+
+/*-------------------------------------------------------*/
+// IMPORT ROUTERS
+/*-------------------------------------------------------*/
+import authRouters from "./src/controllers/authentication/routers.js";
 
 const app = express();
 dotenv.config();
 
 app.use(bodyParser.json());
 app.use(cors({ exposedHeaders: "Authorization" }));
+app.use(middlewares.requestLogger);
 
+/*-------------------------------------------------------*/
+// TEST CONNECTION TO DATABASE
+/*-------------------------------------------------------*/
 async function testConnection() {
   try {
     await db.sequelize.authenticate();
     console.log(
-      "Connection has been established successfully." +
+      chalk.bgGreenBright("Connection with database") +
+        " has been established successfully." +
         emoji.get("white_check_mark")
     );
   } catch (error) {
@@ -28,22 +39,27 @@ async function testConnection() {
 
 testConnection();
 
-// async function syncSequelize() {
-//   try {
-//     associations();
-//     await db.sequelize.sync({ force: true });
-//     console.log(
-//       "All models were synchronized successfully." +
-//         emoji.get("white_check_mark")
-//     );
-//   } catch (error) {
-//     console.error("Unable to synchronize models:", error);
-//   }
-// }
+/*-------------------------------------------------------*/
+// DEFINE ROOT ROUTE
+/*-------------------------------------------------------*/
+app.get("/", (req, res) => {
+  res.status(200).send("<h1> Connected to Digging API successfully. </h1>");
+});
 
-/*=======================================================*/
-//
-/*=======================================================*/
+/*-------------------------------------------------------*/
+// USE ROUTERS
+/*-------------------------------------------------------*/
+app.use("/api/auth", authRouters);
+
+/*-------------------------------------------------------*/
+// USE ERROR HANDLER
+/*-------------------------------------------------------*/
+app.use(middlewares.errorHandler);
+/*-------------------------------------------------------*/
+// LISTEN TO PORT
+/*-------------------------------------------------------*/
 
 const PORT = process.env.PORT;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () =>
+  console.log(chalk.bgGreenBright("Server running") + ` on port ${PORT}`)
+);
